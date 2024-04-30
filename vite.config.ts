0299689eefbfaf96cc935eb7b75a247342facaa9
Michael <michaellogan7860@gmail.com>
2024-04-30 10:39:16 -0500

    More tweaks to config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/MarkdownPreviewer/",
-  plugins: [react()],
+  plugins: [react()]
 })
